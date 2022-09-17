a0e3891d35a1ba7e518da187ceca6aefffdcbb19
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-09-18 01:07:52 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,8 +21,23 @@ class App extends Component {
     });
   };
 
+  countTotalFeedback = () => {
+    const arr = Object.values(this.state);
+    const total = arr.reduce((acc, arr) => acc + Number(arr), 0);
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const arr = Object.values(this.state);
+    const positive = Number(this.state.good);
+    const total = arr.reduce((acc, arr) => acc + Number(arr), 0);
+    const percentPositive = (positive / total) * 100;
+    return Math.round(percentPositive);
+  };
+
   render() {
     const feedbackLabels = Object.keys(this.state);
+    const feedback = this.countPositiveFeedbackPercentage();
 
     return (
       <div>
@@ -33,6 +48,18 @@ class App extends Component {
               <Button label={label} addFeedback={this.addFeedback} />
             </li>
           ))}
+        </ul>
+        <h2>Statistics</h2>
+        <ul>
+          {feedbackLabels.map((label, index) => (
+            <li key={index}>
+              <p>
+                {[label]}: {this.state[label]}
+              </p>
+            </li>
+          ))}
+          <li>total: {this.countTotalFeedback()}</li>
+          <li>positive feedback: {isNaN(feedback) ? '0' : feedback}%</li>
         </ul>
       </div>
     );
