a95d7325bb614336ee2480ee6eda2b359fda91e7
user <danchenkoevhenii@gmail.com>
2022-10-27 12:23:36 +0300

    add p

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -72,7 +72,7 @@ export default function App() {
             />
         )
           : (
-            'There is no feedback'
+            <p>'There is no feedback'</p>
           )
         }
         </Section>
